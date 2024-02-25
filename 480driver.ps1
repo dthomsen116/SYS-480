355352e92151574812fa7c80c17387d6ade06b18
dthomsen116 <david.thomsen@mymail.champlain.edu>
2024-02-25 18:39:44 -0500

    Finishing up Mile 5

--- 480driver.ps1
+++ 480driver.ps1
@@ -9,5 +9,30 @@ $conf=Get-480Config -config_path "/home/david/Documents/SYS-480/480.json"
 #connect to the server
 Connect-480 -server $conf.server
 
-#select the VM
-Select-VM -folder "BaseVMS"
+#Menu
+Write-Host -ForegroundColor Cyan "1. Clone a VM"
+Write-Host -ForegroundColor Cyan "2. Delete a VM"
+Write-Host -ForegroundColor Cyan "3. Switch A VM's Network"
+Write-Host -ForegroundColor Cyan "4. Edit Powerstate of a VM"
+Write-Host -ForegroundColor Red "5. Exit"
+
+$ans = Read-Host "Select an option:"
+
+if($ans -eq 1){
+ CreateClone
+}
+if($ans -eq 2){
+ Terminate-VM
+}
+if($ans -eq 3){
+ changeNetwork
+}
+if($ans -eq 4){
+ editPower
+}
+if($ans -eq "exit" -or $ans -eq "2"){
+    exit
+}
+else{
+    continue
+}
--- Scripts/modules/480-utils/480-utils.psm1
+++ Scripts/modules/480-utils/480-utils.psm1
@@ -69,7 +69,30 @@ function Get-480Config([string] $config_path){
     return $conf
 }
 
-function Select-VM([string] $folder){
+function Select-Folder(){
+    try{
+        $folder = $null
+        $folders = Get-Folder | Where-Object { $_.Name -in @("BaseVMS", "ProdEnv") }
+        $index = 1
+        Write-Host ""
+        Write-Host "Select a Folder" 
+        foreach ($folder in $folders){
+            Write-Host -ForegroundColor DarkCyan [$index] $folder.Name
+            $index++
+        }
+        Write-Host ""
+        $selection = Read-Host "Select a Folder by number"
+        $folder = $folders[$selection-1]
+        Write-Host ""
+        Write-Host -ForegroundColor Green "Selected Folder: $($folder.Name)"
+        return $folder
+    }
+    catch{
+        Write-Host -ForegroundColor Red "Error selecting Folder"
+        return $null
+    }
+}
+function Select-VM(){
     try{
         $vm=$null
         $vms = Get-VM -Location $folder
@@ -77,7 +100,7 @@ function Select-VM([string] $folder){
         Write-Host ""
         Write-Host "Select a VM" 
         foreach ($vm in $vms){
-            Write-Host -ForegroundColor DarkCyan [$index] $vm.Name
+            Write-Host -ForegroundColor Cyan [$index] $vm.Name
             $index++
             }
         Write-Host ""
@@ -93,4 +116,162 @@ function Select-VM([string] $folder){
     }
 }
 
+function Select-Datastore(){
+    try{
+        $ds=$null
+        $ds = Get-Datastore
+        $ds = $ds | Sort-Object Name
+        $index = 1
+        Write-Host ""
+        Write-Host "Select a Datastore" 
+        foreach ($d in $ds){
+            Write-Host -ForegroundColor Cyan [$index] $d.Name " -- Free Space: $($d.FreeSpace/1GB)GB"
+            $index++
+            }
+        Write-Host ""
+        $selection = Read-Host "Select a Datastore by number"
+        $ds = $ds[$selection-1]
+        Write-Host ""
+        Write-Host -ForegroundColor Green "Selected Datastore: $($ds.Name)"
+        return $ds
+        }
+    catch{
+        Write-Host -ForegroundColor Red "Error selecting Datastore"
+        return $null
+    }
+}
 
+function Select-Snapshot(){
+    try{
+        $snap=$null
+        $snap = Get-Snapshot -VM $vm
+        $snap = $snap | Sort-Object Created
+        $index = 1
+        Write-Host ""
+        Write-Host "Select a Snapshot" 
+        foreach ($s in $snap){
+            Write-Host -ForegroundColor DarkCyan [$index] $s.Name " -- Created: $($s.Created)"
+            $index++
+            }
+        Write-Host ""
+        $selection = Read-Host "Select a Snapshot by number"
+        $snap = $snap[$selection-1]
+        Write-Host ""
+        Write-Host -ForegroundColor Green "Selected Snapshot: $($snap.Name)"
+        return $snap
+        }
+    catch{
+        Write-Host -ForegroundColor Red "Error selecting Snapshot"
+        return $null
+    }
+}
+
+function CreateClone(){
+    Write-Host -ForegroundColor Cyan "Clone Type:"
+    Write-Host -ForegroundColor DarkCyan "1 - Full Clone"
+    Write-Host -ForegroundColor DarkCyan "2 - Linked Clone"
+    $clone_type= Read-Host "Select a Clone Type by number"
+
+    if ($clone_type -eq "1"){
+        $clone_name = Read-Host "Enter the name of the new clone"
+        $folder=Select-Folder
+        $vm=Select-VM
+        $ds=Select-Datastore
+        $snap=Select-Snapshot
+        if ($clone_type -eq "1"){
+            New-VM -Name $clone_name -VM $vm -Datastore $ds -VMHost "192.168.7.28" -Location $folder -RunAsync
+            Write-Host -ForegroundColor Green "Full Clone created: $clone_name"
+        }
+    }
+    elseif ($clone_type -eq "2"){
+        $clone_name = Read-Host "Enter the name of the new clone"
+        $folder=Select-Folder
+        $vm=Select-VM
+        $ds=Select-Datastore
+        $snap=Select-Snapshot
+        New-VM -Name $clone_name -VM $vm -Datastore $ds -VMHost "192.168.7.28" -Location $folder -LinkedClone -ReferenceSnapshot $snap
+    }
+    else{
+        continue
+    }
+}
+
+function Terminate-VM(){
+    $folder = Select-Folder
+    if ($folder -eq "ProdEnv"){
+        Write-Host -ForegroundColor Red "You cannot delete VMs from the ProdEnv folder"
+        exit
+    }
+    else{
+        $vm = Select-VM
+        $confirmation = Read-Host "Are you sure you want to delete $($vm.Name)? (yes/no)"
+        if ($confirmation -eq "yes" -or $confirmation -eq "y"){
+            Remove-VM -VM $vm -DeleteFromDisk -Confirm:$false
+            Write-Host -ForegroundColor Green "VM $($vm.Name) deleted"
+        }
+        else{
+            Write-Host -ForegroundColor Red "VM $($vm.Name) not deleted"
+        }
+    }
+}
+
+function Select-Network(){
+    try{
+        $nw=$null
+        $nw = Get-NetworkAdapter -VM $vm
+        $nw = $nw | Sort-Object Name
+        $index = 1
+        Write-Host ""
+        Write-Host "Select a Network" 
+        foreach ($n in $nw){
+            Write-Host -ForegroundColor Cyan [$index] $n.NetworkName
+            $index++
+            }
+        Write-Host ""
+        $selection = Read-Host "Select a Network by number"
+        $nw = $nw[$selection-1]
+        Write-Host ""
+        Write-Host -ForegroundColor Green "Selected Network: $($nw.NetworkName)"
+        return $nw
+        }
+    catch{
+        Write-Host -ForegroundColor Red "Error selecting Network"
+        return $null
+    }
+}
+
+function changeNetwork(){
+    $vm=Select-VM
+    $nw=Select-Network
+    Set-NetworkAdapter -NetworkAdapter $nw -Connected:$true -Confirm:$false
+    Write-Host -ForegroundColor Green "Network changed for $($vm.Name)"
+}
+
+function editPower(){
+    $vm=Select-VM
+        if ($ps -eq "PoweredOff"){
+            $ps = $vm.PowerState
+            $wps = Write-Host -ForegroundColor Red "Power state: $ps"
+            Write-host -ForegroundColor Cyan "Current powerstate of $($vm.Name): $wps"
+        }
+        else{
+            $ps = $vm.PowerState
+            $wps = Write-Host -ForegroundColor Green "Power state: $ps"
+            Write-host -ForegroundColor Cyan "Current powerstate of $($vm.Name): $wps"
+        }    
+    Write-Host -ForegroundColor Cyan "Power State:"
+    Write-Host -ForegroundColor DarkCyan "1 - Power On"
+    Write-Host -ForegroundColor DarkCyan "2 - Power Off"
+    $power_state= Read-Host "Select a Power State by number"
+        if ($power_state -eq "1"){
+            Start-VM -VM $vm -Confirm:$false
+            Write-Host -ForegroundColor Green "VM $($vm.Name) powered on"
+        }
+        elseif ($power_state -eq "2"){
+            Stop-VM -VM $vm -Confirm:$false
+            Write-Host -ForegroundColor Green "VM $($vm.Name) powered off"
+        }
+        else{
+            continue
+        }
+}
